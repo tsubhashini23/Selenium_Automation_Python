df215043ff8adf397fb9d273f374738ec7ded543
tsubhashini23 <tsubhashini23@gmail.com>
2025-09-12 17:46:13 +0530

    find_elements, select class

--- xpath_locators.py
+++ xpath_locators.py
@@ -1,9 +1,14 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import Select
 
 driver = webdriver.Chrome()
 driver.get("https://demo.automationtesting.in/Index.html")
+driver.maximize_window()
+print("Page title: ", driver.title)
+driver.implicitly_wait(2)
 
 driver.find_element(By.ID, "email").send_keys("tsubha1@gmail.com")
 driver.find_element(By.ID, "enterimg").click()
@@ -45,5 +50,49 @@ driver.find_element(By.XPATH, "//*[@placeholder='Last Name']").send_keys("Thirum
 # //div[@placeholder='Last Name']/parent::form
 
 driver.find_element(By.XPATH, "//*[@type='email' and @ng-model='EmailAdress']").send_keys("test@gmailcom")
+
+driver.find_element(By.XPATH, "//input[@value= 'FeMale']")
+
+driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+
+
+#Select Multiple checkbox, find_elements
+
+element_list = driver.find_elements(By.XPATH, "//input[@type = 'checkbox']")
+
+for element in element_list:
+    val = element.get_attribute('value')
+    print(val)
+    # if val == 'Movies':
+    element.click()
+
+
+
+# Select class
+dropdown_element = driver.find_element(By.XPATH, "//select[@id = 'Skills']")
+click_dropdown_element = Select(dropdown_element)
+
+click_dropdown_element.select_by_index(1)
+
+click_dropdown_element.select_by_value('Android')
+
+click_dropdown_element.select_by_visible_text('Art Design')
+
+print(driver.current_url)
+
+driver.get("https://www.google.com/?zx=1757678760816&no_sw_cr=1")
+
+print(driver.current_url)
+
+driver.back()
+
+print(driver.current_url)
+
+driver.refresh()
+
+driver.forward()
+
+print(driver.current_url)
+
 
 driver.quit()
